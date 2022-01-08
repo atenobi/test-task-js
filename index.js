b2f6324dbb1c206f954edecbf1a37d550059b1d9
atenobi <79144475+atenobi@users.noreply.github.com>
2022-01-08 16:38:42 +0300

    final wersion

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
 // main container
-const divContainer = document.getElementsByClassName('collapsible').item(0);
+const divContainer = document.querySelector('.collapsible');
 
-// little style for container just for easier read
+// little style for container just easier to work
 divContainer.style.display = 'flex';
 divContainer.style.flexDirection = 'column';
 divContainer.style.alignItems = 'center';
 
 // DOM elements
-const collapseButtonEl = document.getElementsByTagName('button').item(0);
-const spanShowButtonEl = document.getElementsByClassName('collapsible__action').item(1);
-const spanHideButtonEl = document.getElementsByClassName('collapsible__action').item(0);
-const textContentPEl = document.getElementsByClassName('collapsible__content').item(0);
+const collapseButtonEl = document.querySelector('.collapsible__button');
+const spanButtonElArr = document.querySelectorAll('.collapsible__action');
+const textContentPEl = document.querySelector('.collapsible__content');
+
+const spanShowButtonEl = spanButtonElArr[1];
+const spanHideButtonEl = spanButtonElArr[0];
 
 // default button text
 collapseButtonEl.textContent = spanShowButtonEl.textContent;
 
 // content default visibility
-textContentPEl.style.visibility = 'hidden';
+textContentPEl.style.opacity = '0';
 
-// function for change button value and hide content
+// function for change button value / hide content / animate content
 const collapseButtonFunction = (e) => {
 	e.preventDefault();
-	if(e.target.textContent === spanHideButtonEl.textContent) {
+	if (e.target.textContent === spanHideButtonEl.textContent) {
 		e.target.textContent = spanShowButtonEl.textContent
-		textContentPEl.style.visibility = 'hidden';
+
+		textContentPEl.animate([
+			{ opacity: '1', },
+			{ opacity: '0', },
+		], {
+			duration: 1000,
+			iterations: 1,
+			easing: 'ease-in-out',
+		}).finished.then(()=>{
+				textContentPEl.style.opacity = '0';
+			});
 	} else {
 		e.target.textContent = spanHideButtonEl.textContent
-		textContentPEl.style.visibility = 'visible';
+
+		textContentPEl.animate([
+			{ opacity: '0', },
+			{ opacity: '1', },
+		], {
+			duration: 1000,
+			iterations: 1,
+		}).finished.then(()=> {
+			textContentPEl.style.opacity = '1';
+		})
 	}
 }
 
